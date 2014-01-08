d8b1278f18171e24b2291268a22ac6f1020ca002
Kirill Borisov <lensvol@gmail.com>
2014-01-09 01:11:53 +0400

    Basic curses implementation.

--- gunitop/main.py
+++ gunitop/main.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 
+import curses
 import collections
 import json
 import socket
@@ -7,10 +8,89 @@ import sys
 import threading
 import time
 
+# for keeping track of y line offsets
+from itertools import count
+
 workers = {}
 # In seconds
 PERIOD = 1
+LEFT_BORDER_OFFSET = 3
+BORDER_SPACING = 1
 
+class MonitorWindow(object):
+    win = None
+    exiting = False
+    screen_delay = PERIOD
+    foreground = curses.COLOR_GREEN
+    background = curses.COLOR_BLACK
+
+    def __init__(self, workers):
+        self.workers = workers
+
+    def handle_keypress(self):
+        try:
+            key = self.win.getkey().upper()
+        except:
+            return
+
+        if key == 'Q':
+            self.exiting = True
+
+    def draw(self):
+        win = self.win
+        self.handle_keypress()
+        x = LEFT_BORDER_OFFSET
+        blank_line = y = count(1).next
+        mx, my = win.getmaxyx()
+        win.erase()
+        win.bkgd(' ', curses.color_pair(1))
+        win.border()
+        win.addstr(y(), x, '{}{}{}'.format('PID'.center(6), ' '*3, 'STATUS'), curses.color_pair(1))
+
+        for pid, w in self.workers.iteritems():
+            win.addstr(y(), x, '{}{}{}'.format(str(pid).center(6), ' '*3, w['status'][:mx-x-6]),
+                                               curses.A_BOLD)
+
+        win.hline(2, 1, curses.ACS_HLINE, self.screen_width - 1)
+        win.vline(1, x + 7, curses.ACS_VLINE, self.screen_height - 2)
+        win.refresh()
+
+    def init_screen(self):
+        self.win = curses.initscr()
+        self.win.nodelay(True)
+        self.win.keypad(True)
+        curses.start_color()
+        curses.init_pair(1, self.foreground, self.background)
+        curses.cbreak()
+
+    def resetscreen(self):
+        curses.nocbreak()
+        self.win.keypad(False)
+        curses.echo()
+        curses.endwin()
+
+    def nap(self):
+        curses.napms(self.screen_delay)
+
+    @property
+    def screen_width(self):
+        _, mx = self.win.getmaxyx()
+        return mx
+
+    @property
+    def screen_height(self):
+        my, _ = self.win.getmaxyx()
+        return my
+
+    @property
+    def display_width(self):
+        _, mx = self.win.getmaxyx()
+        return mx - BORDER_SPACING
+
+    @property
+    def display_height(self):
+        my, _ = self.win.getmaxyx()
+        return my - 7
 
 class ListenerThread(threading.Thread):
 
@@ -20,6 +100,7 @@ class ListenerThread(threading.Thread):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         host = socket.gethostbyname(host)
         self.socket.bind((host, port))
+        self.socket.setblocking(False)
         threading.Thread.__init__(self)
 
     def process(self, text):
@@ -34,7 +115,7 @@ class ListenerThread(threading.Thread):
 
         if info_type == 'spawn':
             worker['ppid'] = info['worker']['ppid']
-            worker['status'] = u'Freshly spawned!'
+            worker['status'] = u'Waiting...'
         elif info_type == 'request':
             worker['status'] = '{0} {1}'.format(info['method'], info['path'])
         elif info_type == 'response':
@@ -43,20 +124,32 @@ class ListenerThread(threading.Thread):
 
     def run(self):
         while not self.stop:
-            text, source = self.socket.recvfrom(65535)
-            self.process(text)
+            try:
+                text, source = self.socket.recvfrom(8192)
+            except:
+                pass
+            else:
+                self.process(text)
+            time.sleep(PERIOD)
 
 def main():
     listener = ListenerThread(workers)
     listener.start()
 
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+
     try:
-        while True:
+        monitor = MonitorWindow(workers)
+        monitor.init_screen()
+        while not monitor.exiting:
             time.sleep(PERIOD)
-            for pid, w in workers.items():
-                print '{0} {1}'.format(pid, w['status'])
-            print ''
-            sys.stdout.flush()
+            monitor.draw()
+            monitor.nap()
+        monitor.resetscreen()
     except (KeyboardInterrupt, SystemExit):
-        listener.stop = True
-        listener.join()
+        pass
+
+    print 'Waiting for listener to stop...'
+    listener.stop = True
+    listener.join()
